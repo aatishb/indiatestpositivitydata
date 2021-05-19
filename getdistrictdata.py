b02372aa223d2d0629e08c95c44c83e351b82e70
Aatish Bhatia <aatish@gmail.com>
2021-05-19 16:43:30 -0400

    added script to collect state data

--- getdistrictdata.py
+++ getdistrictdata.py
@@ -1,5 +1,5 @@
-# script to parse COVID test positivity data from https://www.mohfw.gov.in/
-# the data is appended to data.csv and archived in the archive folder
+# script to parse India district level COVID test positivity data from https://www.mohfw.gov.in/
+# the data is appended to districtdata.csv and archived in the archive folder
 
 import pandas as pd
 from datetime import datetime
@@ -10,35 +10,35 @@ from urllib.error import HTTPError
 # get current date in IST
 IST = pytz.timezone('Asia/Kolkata')
 today = datetime.now(IST)
-currentdate = today.isoformat().split('T')[0]
+print('checking for new district data on', today)
+
+currentdate = today.date()
 # https://stackoverflow.com/a/20007730
 ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n//10%10!=1)*(n%10<4)*n%10::4])
 monthname = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-date = ordinal(today.day) + monthname[today.month - 1]
-
-# parse URL for that date
-print('checking for new data on', today)
+date = ordinal(currentdate.day) + monthname[currentdate.month - 1]
 
 try:
     url = 'https://www.mohfw.gov.in/pdf/10percentDistrictWiseCOVID19Positivity' + date + '.xlsx'
     df = pd.read_excel(url, engine = 'openpyxl', skiprows=[0,1,2], header=[0]).fillna(method='ffill')
+    df = df.rename(columns = {'Positivity': 'Test Positivity Rate'})
     df.drop(df.tail(1).index,inplace=True)
     df['Date'] = currentdate
 
-    csv = pd.read_csv('data.csv', header=None)
+    csv = pd.read_csv('districtdata.csv', header=0)
     prevdate = csv.iloc[-1,0]
 
     if (prevdate != currentdate):
-        df.to_csv("data.csv", columns = ['Date', 'State', 'District', 'Positivity'], header = False, index = False, mode='a')
-        print('added data for', currentdate, 'to data.csv')
+        df.to_csv("districtdata.csv", columns = ['Date', 'State', 'District', 'Test Positivity Rate'], header = False, index = False, mode='a')
+        print('added district data for', currentdate, 'to districtdata.csv')
         path = 'archive/' + url.split('/')[-1]
         request.urlretrieve(url, path)
-        print('saved data for', currentdate, 'to archive folder')
+        print('saved district data for', currentdate, 'to archive folder')
 
     else:
-        print('already have data for', currentdate)
+        print('already have district data for', currentdate)
         
 except HTTPError as err:
     print(err)
     if err.code == 404:
-            print('data has not yet been updated for', currentdate)
+            print('district data has not yet been updated for', currentdate)
---
+++ getstatedata.py
@@ -0,0 +1,76 @@
+# script to calculate India state level test positivity data
+# state case data https://api.covid19india.org/csv/latest/states.csv
+# state testing data https://api.covid19india.org/csv/latest/statewise_tested_numbers_data.csv
+# the data is output to statedata.csv
+
+import pandas as pd
+from datetime import date, datetime, timedelta
+import pytz
+from urllib.error import HTTPError
+
+# https://www.w3resource.com/python-exercises/date-time-exercise/python-date-time-exercise-50.php
+def daterange(date1, date2):
+    for n in range(int ((date2 - date1).days)+1):
+        yield date1 + timedelta(n)
+
+states = ['Andaman and Nicobar Islands', 'Andhra Pradesh',
+       'Arunachal Pradesh', 'Assam', 'Bihar', 'Chandigarh',
+       'Chhattisgarh', 'Dadra and Nagar Haveli and Daman and Diu',
+       'Delhi', 'Goa', 'Gujarat', 'Haryana', 'Himachal Pradesh', 'India',
+       'Jammu and Kashmir', 'Jharkhand', 'Karnataka', 'Kerala', 'Ladakh',
+       'Lakshadweep', 'Madhya Pradesh', 'Maharashtra', 'Manipur',
+       'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha', 'Puducherry',
+       'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Nadu', 'Telangana', 
+       'Tripura', 'Uttar Pradesh', 'Uttarakhand', 'West Bengal']
+
+IST = pytz.timezone('Asia/Kolkata')
+today = datetime.now(IST)
+print('checking for new state data on', today)
+
+try:
+  cases = pd.read_csv('https://api.covid19india.org/csv/latest/states.csv')
+  cases.index = pd.to_datetime(cases['Date'], dayfirst = True)
+
+  tests = pd.read_csv('https://api.covid19india.org/csv/latest/statewise_tested_numbers_data.csv')
+  tests.index = pd.to_datetime(tests['Updated On'], dayfirst = True)
+
+  df = pd.DataFrame(columns = ['Date' , 'State', 'Weekly Cases' , 'Weekly Tests', 'Test Positivity Rate'])
+
+  for dt in daterange(date(2021, 5, 1), today.date()):
+      for state in states:
+
+          datestring = dt.strftime("%Y-%m-%d")
+          
+          statecases = cases[cases['State'] == state]['Confirmed']
+          stateweeklycases = statecases - statecases.shift(7)
+
+          statetests = tests[tests['State'] == state]['Total Tested']
+          stateweeklytests = statetests - statetests.shift(7)
+
+          currentstateweeklycases = stateweeklycases[stateweeklycases.index == datestring]
+          currentstateweeklytests = stateweeklytests[stateweeklytests.index == datestring]
+
+          if (currentstateweeklycases.size == 1 and currentstateweeklytests.size == 1):
+              if (currentstateweeklycases.iloc[0] > 0 and currentstateweeklytests.iloc[0] > 0):
+                  #print(state, str(round(100 * currentstateweeklycases.iloc[0] / currentstateweeklytests.iloc[0])) + '%')
+                  df = df.append({'Date': datestring,
+                             'State': state,
+                             'Weekly Cases': round(currentstateweeklycases.iloc[0]),
+                             'Weekly Tests': round(currentstateweeklytests.iloc[0]),
+                             'Test Positivity Rate': round(currentstateweeklycases.iloc[0] / currentstateweeklytests.iloc[0],3)
+                            }, ignore_index=True)
+
+
+  csv = pd.read_csv('statedata.csv', header=0)
+  datachanged = not (csv.round(3) == df.round(3)).all().all()
+  
+  if datachanged:
+    df.to_csv("statedata.csv", columns = ['Date', 'State', 'Weekly Cases', 'Weekly Tests', 'Test Positivity Rate'], header = True, index = False)
+    print('added state data to statedata.csv on', today.date())
+  else:
+    print('no new state data found on', today.date())
+
+except HTTPError as err:
+    print(err)
+    if err.code == 404:
+            print('could not access state data')
